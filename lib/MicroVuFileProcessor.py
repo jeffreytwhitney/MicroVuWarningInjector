e141e1caf5fce609855cd8fe4c1086e81ec38adf
jeffreytwhitney <jeffrey.t.whitney@gmail.com>
2024-02-06 12:12:25 -0600

    init

--- lib/MicroVuFileProcessor.py
+++ lib/MicroVuFileProcessor.py
@@ -32,17 +32,24 @@ class CoonRapidsProcessor:
         micro_vu.insert_line(bring_to_met_pic_idx, self.bring_part_to_metrology_lines[1])
 
     def _write_file_to_harddrive(self, micro_vu: MicroVuProgram) -> None:
-        with open(micro_vu.filepath, 'w', encoding='utf-16-le', newline='\r\n') as f:
-            for line in micro_vu.file_lines:
-                f.write(f"{line}")
+
+        try:
+            with open(micro_vu.filepath, 'w', encoding='utf-16-le', newline='\r\n') as f:
+                for line in micro_vu.file_lines:
+                    f.write(f"{line}")
+        except:
+            pass
 
     def process_files(self) -> None:
         input_rootpath = lib.Utilities.GetStoredIniValue("Paths", "input_rootpath", "Settings")
 
         for root, directory, files in os.walk(input_rootpath):
             for file in files:
+                if not file.upper().endswith(".IWP"):
+                    continue
                 filepath = str(os.path.join(root, file))
                 micro_vu = MicroVuProgram(filepath)
+                print(micro_vu.filepath)
                 if micro_vu.has_bring_to_metrology_picture:
                     continue
                 if micro_vu.has_been_converted:
